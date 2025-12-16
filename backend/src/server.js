44c14ab8d1724e1040001d22edcab2e1f23c227d
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-16 21:07:10 +0400

    fix: backend cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,32 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 
-import { connectDB } from './config/db.js';
-
-import dotenv from 'dotenv';
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 5001;
+
+// middleware
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    })
+  );
+}
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
 app.use('/api/notes', notesRoutes);
 
