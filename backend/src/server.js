90681d81e2eca6132fb75788d80dca45f42904a5
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-16 10:00:19 +0400

    build: server.js for the routes and config

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import notesRoutes from './routes/notesRoutes.js';
+import rateLimiter from './middleware/rateLimiter.js';
+
+import { connectDB } from './config/db.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(rateLimiter);
+
+app.use('/api/notes', notesRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log('Server started on PORT:', PORT);
+  });
+});
